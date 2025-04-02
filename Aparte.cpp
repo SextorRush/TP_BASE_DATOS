df839e57b6d78d57ff66e00f9d90965cf9d4440b
Paryania <acandela73@gmail.com>
2025-04-02 12:27:00 -0300

    pochita.cpp

--- Aparte.cpp
+++ Aparte.cpp
@@ -14,35 +14,100 @@ struct DatoIndice{
     dato *dir;
 }
 
-
-int main(void){
-    dato TablaDatos[30];
-    DatoIndice TablaIndices[5];
-void insertar (dato a){
-if(TablaIndices[0].clave==0){
-TablaDatos[0].clave=a.clave;
-TablaDatos[0].valor=a.valor;
-TablaDatos[0].dir=nullptr;
-
-TablaIndices[0].claver=a.clave;
-TablaIndices[0].dir=&TablaDatos[0];
-}else if (((TablaIndices[0].clave > a.clave) && (a.clave < TablaIndices[1].clave))||(TablaIndices[0].clave > a.clave) && (TablaIndices[1].clave == 0)){
+void insertar(dato a){
+    //inserta si la tabla de indices esta vacia
+    if(TablaIndices[0].clave==0){
+        TablaDatos[0].clave=a.clave;
+        TablaDatos[0].valor=a.valor;
+        TablaDatos[0].dir=nullptr;
+        TablaIndices[0].claver=a.clave;
+        TablaIndices[0].dir=&TablaDatos[0];}
+    //si la tabla de indices no esta vacia,primer bloque
+    else if(((TablaIndices[0].clave < a.clave))||
+        ((TablaIndices[0].clave > a.clave) && (TablaIndices[1].clave <a.clave))){
+        //si hay 3 lugares disponibles
+            if (EspacioLibre(0)==3){
+            TablaDatos[EspacioOcupado(0)+1].clave=a.clave;
+            TablaDatos[EspacioOcupado(0)+1].valor=a.valor;
+            TablaDatos[EspacioOcupado(0)+1].dir=nullptr;
+            ordenar(0);
+            TablaIndices[0].claver=a.clave;
+            TablaIndices[0].dir=&TablaDatos[0];
+           }
+        //si hay 2 lugares disponibles, subocupado
+           else if (EspacioLibre(0)<3){
+            //si no hay va al overflow
+            if(EspacioLibre(0)==0){
+                enviar_al_overflow();
+                TablaDatos[3].dir=&TablaDatos[20];
+            } 
+            //verifica e inserta insercion media
+            else if(buscar_max(0)==true);{
+                TablaDatos[EspacioOcupado(0)+1].clave=a.clave;
+                TablaDatos[EspacioOcupado(0)+1].valor=a.valor;
+                TablaDatos[EspacioOcupado(0)+1].dir=nullptr;
+                ordenar(0);
+                TablaIndices[0].claver=a.clave;
+                TablaIndices[0].dir=&TablaDatos[0];
+            }
+            //si no es insersion media
+            else if(buscar_max(0)==false){
+               if( EspacioOcupado(0)>=2){
+                TablaDatos[BlokeLibre()*4].clave=a.clave;
+                TablaDatos[BlokeLibre()*4].valor=a.valor;
+                TablaDatos[BlokeLibre()*4].dir=nullptr;
+                TablaIndices[BlokeLibre()].claver=a.clave;
+                TablaIndices[BlokeLibre()].dir=&TablaDatos[0];
+               }
+               }
+            }
+           }
+        }
     
-
-    int pos(){
-        return BlokeLibre() *4;
+//ordena el bloque de la tabla de datos
+    void ordenar(int pos){
+        sort(TablaDatos[pos],TablaDatos[pos]+4);
+        }            
+//busca el maximo del bloque de la tabla de datos
+bool buscar_max(int i){
+    bool flag=false;
+    for(i;i<4;i++){
+        if(TablaDatos[i].clave>a.dato){
+            flag=true;
+            return flag;
+        }
     }
-
-    //Cantidad de elementos del bloque
-int EsacioLibre(int a=pos())
-    int contador_bloke=0;
-    int i=a;
-    for (i;i<4;i++){
-        if(TablaDatos[i].clave==0)contador_bloke ++;
+    return flag;
+}
+//Devuelve espacios libres en la tabla de datos
+    int EspacioLibre(int a){
+        int contador_bloke=0;
+        int i=a;
+        for (i;i<4;i++){
+            if(TablaDatos[i].clave==0)contador_bloke++;
+        }
+        return contador_bloke;}
+//Devuelve espacios ocupados en la tabla de datos
+    int EspacioOcupado(int a){
+        int contador_bloke=0;
+        int i=a;
+        for (i;i<4;i++){
+            if(TablaDatos[i].clave!=0)contador_bloke++;
+        }
+        return contador_bloke;}
+//envia al overflow
+void enviar_al_overflow(){
+    for(int i=20;i<30;i++){
+        if(TablaDatos[i].clave==0){
+            TablaDatos[i].clave=a.clave;
+            TablaDatos[i].valor=a.valor;
+            TablaDatos[i].dir=nullptr;
+        } else {
+            cout<<"Overflow lleno";
+        }
     }
-
-
-//verificar blokes libres
+}
+//verifica bloques libres, devuelve la posición del primer bloque libre
 int BlokeLibre(){
     int aux = 0;
     for(int i=0;i<4;i++){
@@ -55,56 +120,8 @@ int BlokeLibre(){
     return aux;
 }
 
-    
-    if(contador_bloke>2 && BlokeLibre==0){
-        for(int i=0;i<4;i++){
 
-            if(TablaDatos[i].clave==0){
-            TablaDatos[i].clave=a.clave;
-            TablaDatos[i].valor=a.valor;
-            TablaDatos[i].dir=nullptr;
-            break;
-        } else if(i>4){
-            for(int i=20;i<30;i++){
-
-                if(TablaDatos[i].clave==0){
-                TablaDatos[i].clave=a.clave;
-                TablaDatos[i].valor=a.valor;
-                TablaDatos[i].dir=nullptr;
-                break;
-            } 
-        }
-
-    }
-
- }}
-bool buscar_max(int i){
-    bool flag=false;
-    for(i;i<4;i++){
-        if(TablaDatos[i].clave>a.dato){
-            flag=true;
-            return flag;
-        }
-    }
-    return flag;
-}
-
-void ordenar(int pos){
-    sort(TablaDatos[pos],TablaDatos[pos]+4);
-}
-
-}}
-
-void enviar_al_overflow(){
-    for(int i=20;i<30;i++){
-        if(TablaDatos[i].clave==0){
-            TablaDatos[i].clave=a.clave;
-            TablaDatos[i].valor=a.valor;
-            TablaDatos[i].dir=nullptr;
-        } else {
-            cout<<"Overflow lleno";
-        }
-    }
-}
-holaaaaa
+        int main(void){
+    dato TablaDatos[30];
+    DatoIndice TablaIndices[5];
 }
